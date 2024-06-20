806379981c7aa14971c5bca75c7bd47a58d069a0
Youssef Shahien <youssefshahien156@gmail.com>
2024-06-20 08:19:35 +0300

    create Login

--- ecommerce-front/src/routes/AppRouter.tsx
+++ ecommerce-front/src/routes/AppRouter.tsx
@@ -6,6 +6,8 @@ import Home from "@pages/Home";
 import Categories from "@pages/Categories";
 import AboutUs from "@pages/AboutUs";
 import Products from "@pages/Products";
+import Login from "@pages/Login";
+import Register from "@pages/Register";
 
 const router = createBrowserRouter([
   {
@@ -27,6 +29,14 @@ const router = createBrowserRouter([
       {
         path: "about-us",
         element: <AboutUs />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "register",
+        element: <Register />,
       },
     ],
   },
